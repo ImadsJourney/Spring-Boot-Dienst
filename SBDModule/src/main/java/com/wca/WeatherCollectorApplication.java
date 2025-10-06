f8933f031db8f50af6737e3ae1e498842b7af6e9
ImadsJourney <imadaldin2005@gmail.com>
2025-10-06 17:29:01 +0200

    add SpringBoot, Dependencies

---
+++ SBDModule/src/main/java/com/wca/WeatherCollectorApplication.java
@@ -0,0 +1,11 @@
+package com.wca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherCollectorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherCollectorApplication.class, args);
+    }
+}
